108be82b0de93f5982a7a8530a02482682a7b559
Bagas Agung Wiyono <zenkun.enterkill13@gmail.com>
2025-01-31 15:30:26 +0700

    new

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 import {getJSON} from "https://t.if.co.id/714240042/WhatsApp%20Image%202024-10-31%20at%2006.53.28_69d176c5.jpg"
 import {renderHTML} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js"
 
-renderHTML("qr", "/content/content.html")
+renderHTML("qr", "/content/content.html");
 
-getJSON("https://t.if.co.id/json/bagas.json", null, null, rensponseFunction)
+getJSON("https://t.if.co.id/json/bagas.json","null","null",rensponseFunction);
 function rensponseFunction(response) {
   console.log(response)
 }
