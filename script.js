d0d6985bb7815faa1242e1668ea169fb58fc2fa9
Bagas Agung Wiyono <zenkun.enterkill13@gmail.com>
2025-01-31 16:48:26 +0700

    test

--- script.js
+++ script.js
@@ -8,29 +8,29 @@ function responseFunction(response) {
   setInner('nama', response.name);
   console.log('HTTP Status:', response.status);
   console.log('Response Data:', response.data);
-  let cardname;
-  cardname = response.data;
-  const isicard = document.getElementById("card");
-  cardname.forEach((card) => {
-    // Membuat elemen div untuk setiap item
-    const teamContainer = document.createElement("div");
-    teamContainer.classList.add("team-list");
+  // let cardname;
+  // cardname = response.data;
+  // const isicard = document.getElementById("card");
+  // cardname.forEach((card) => {
+  //   // Membuat elemen div untuk setiap item
+  //   const teamContainer = document.createElement("div");
+  //   teamContainer.classList.add("team-list");
 
-    // Membuat elemen deskripsi
-    const pnama = document.createElement("p");
+  //   // Membuat elemen deskripsi
+  //   const pnama = document.createElement("p");
 
-    const link = document.createElement("a");
-    link.textContent = team.nama;
-    link.href = team.link;
-    console.log(pnama);
-    console.log(teamContainer);
-    pnama.appendChild(link);
-    // Menambahkan icon, judul, dan deskripsi ke dalam layananItem
-    teamContainer.appendChild(pnama);
+  //   const link = document.createElement("a");
+  //   link.textContent = team.nama;
+  //   link.href = team.link;
+  //   console.log(pnama);
+  //   console.log(teamContainer);
+  //   pnama.appendChild(link);
+  //   // Menambahkan icon, judul, dan deskripsi ke dalam layananItem
+  //   teamContainer.appendChild(pnama);
 
-    // Menambahkan layananItem ke container
-    isiteam.appendChild(teamContainer);
+  //   // Menambahkan layananItem ke container
+  //   isiteam.appendChild(teamContainer);
 
 
-  });
+  // });
 }
