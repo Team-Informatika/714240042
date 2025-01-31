822be5870aa606b6034fbe1cce453fb31a0a38c2
Bagas Agung Wiyono <zenkun.enterkill13@gmail.com>
2025-01-31 16:16:39 +0700

    test

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
 import { getJSON } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/api.js"
-import { renderHTML } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js"
+import { renderHTML, setInner} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js"
 
-renderHTML('qr', 'content.html');
+renderHTML('card', 'content.html');
 
 getJSON("https://t.if.co.id/json/bagas.json","null","null",responseFunction);
 function responseFunction(response) {
+  setInner('nama', response.card.details.name);
   console.log('HTTP Status:', response.status);
   console.log('Response Data:', response.data);
+  let cardname;
+  cardname = response.data;
+  const isicard = document.getElementById("card");
+  cardname.forEach((card) => {
+    // Membuat elemen div untuk setiap item
+    const teamContainer = document.createElement("div");
+    teamContainer.classList.add("team-list");
+
+    // Membuat elemen deskripsi
+    const pnama = document.createElement("p");
+
+    const link = document.createElement("a");
+    link.textContent = team.nama;
+    link.href = team.link;
+    console.log(pnama);
+    console.log(teamContainer);
+    pnama.appendChild(link);
+    // Menambahkan icon, judul, dan deskripsi ke dalam layananItem
+    teamContainer.appendChild(pnama);
+
+    // Menambahkan layananItem ke container
+    isiteam.appendChild(teamContainer);
+
+
+  });
 }
