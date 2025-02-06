d12f7a7505be0b91d7f0b282cc76fdf70be7229c
Bagas Agung Wiyono <zenkun.enterkill13@gmail.com>
2025-02-06 11:34:26 +0700

    test

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ getJSON("https://t.if.co.id/json/bagas.json","null","null",responseFunction);
 function responseFunction(response) {
   setInner('nama', response.data.card.details.name);
   setInner('occupation', response.data.card.details.occupation)
-  const avatarSrc = data.card.avatar.src;
+  const avatarSrc = response.data.card.avatar.src;
   const avatarHTML = `<img src="${avatarSrc}" alt="Avatar">`;
   setInner('avatar', avatarHTML);
   
