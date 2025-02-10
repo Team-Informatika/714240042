078f307739fc834d2e4769f4e350218a81471cb2
Bagas Agung Wiyono <zenkun.enterkill13@gmail.com>
2025-02-10 14:49:54 +0700

    test

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ import { getJSON } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/api.js"
 import { renderHTML, setInner } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js"
 import { getHash, onHashChange } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/url.js"
 
-// onHashChange(muncul);
+onHashChange(muncul);
 
-// renderHTML('qr', 'qr.html')
+renderHTML('qr', 'qr.html')
 
-// function muncul() {
-//   console.log(getHash());
-//   const hashpath = getHash();
-//   if (hashpath === 'content') {
-//     console.log("kedetek");
-//     renderHTML('cardmuncul', "content.html", () => {
-//       console.log("cardmuncul selesai, lanjut render cardbenar");
-//       renderHTML('cardbenar', "content.html");
-//     });
-//   }
-// }
-renderHTML('cardbenar', 'content.html')
-getJSON("https://t.if.co.id/json/bagas.json", null, null, responseFunction);
+function muncul() {
+  console.log(getHash());
+  const hashpath = getHash();
+  if (hashpath === 'content') {
+    console.log("kedetek");
+    renderHTML('cardmuncul', "content.html", renderDataKartu);
+  }
+}
+function renderDataKartu() {
+  getJSON("https://t.if.co.id/json/bagas.json", null, null, responseFunction);
+}
+// renderHTML('cardbenar', 'content.html')
 
 function responseFunction(response) {
+  renderHTML('cardbenar', 'content.html');
   console.log('HTTP Status:', response.status);
   console.log('Response Data:', response.data);
   setInner('nama', response.data.card.details.name);
